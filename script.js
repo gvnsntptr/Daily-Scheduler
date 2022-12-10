c3e48e79ee41187316cf285af5cb07e2d26f1876
Gavin <gsantopetro.contact@gmail.com>
2022-12-10 14:47:20 -0600

    Set interval function

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 $(document).ready(function () {
-    // listen for save button clicks
+    
     $('.saveBtn').on('click', function () {
-      // get nearby values
-      var value = $(this).siblings('.description').val();
+
+      var val = $(this).siblings('.description').val();
       var time = $(this).parent().attr('id');
   
-      // save in localStorage
-      localStorage.setItem(time, value);
+   
+      localStore.setItem(time, val);
   
-      // Show notification that item was saved to localStorage by adding class 'show'
+
       $('.notification').addClass('show');
-  
-      // Timeout to remove 'show' class after 5 seconds
+
       setTimeout(function () {
         $('.notification').removeClass('show');
       }, 5000);
     });
   
     function hourUpdater() {
-      // get current number of hours
-      var currentHour = moment().hours();
+
+      var currentHr = moment().hours();
   
-      // loop over time blocks
+
       $('.time-block').each(function () {
-        var blockHour = parseInt($(this).attr('id').split('-')[1]);
+        var blockHr = parseInt($(this).attr('id').split('-')[1]);
   
-        // check if we've moved past this time
-        if (blockHour < currentHour) {
+  
+        if (blockHr < currentHr) {
           $(this).addClass('past');
-        } else if (blockHour === currentHour) {
+        } else if (blockHr === currentHr) {
           $(this).removeClass('past');
           $(this).addClass('present');
         } else {
@@ -41,20 +40,18 @@ $(document).ready(function () {
   
     hourUpdater();
   
-    // set up interval to check if current time needs to be updated
-    var interval = setInterval(hourUpdater, 15000);
+    var setInterval = setInterval(hourUpdater, 15000);
   
-    // load any saved data from localStorage
-    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
-    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
-    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
-    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
-    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+    $('#hr-9 .description').val(localStore.getItem('hr-9'));
+    $('#hr-10 .description').val(localStore.getItem('hr-10'));
+    $('#hr-11 .description').val(localStore.getItem('hr-11'));
+    $('#hr-12 .description').val(localStore.getItem('hr-12'));
+    $('#hr-13 .description').val(localStore.getItem('hr-13'));
+    $('#hr-14 .description').val(localStore.getItem('hr-14'));
+    $('#hr-15 .description').val(localStore.getItem('hr-15'));
+    $('#hr-16 .description').val(localStore.getItem('hr-16'));
+    $('#hr-17 .description').val(localStore.getItem('hr-17'));
   
-    // display current day on page
+
     $('#currentDay').text(moment().format('dddd, MMMM Do'));
   });
